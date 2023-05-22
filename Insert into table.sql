9fbd575b0054e1a311cd0433f8e50580ed140a85
Reddappa M R <113326081+Reddappa-M-R@users.noreply.github.com>
2023-05-22 22:52:49 +0530

    second commit
    Inert into table,
    null values

---
+++ Insert into table.sql
@@ -0,0 +1,13 @@
+INSERT INTO employees
+VALUES  (2, "Rckey", "Rddy", 51.40, "2023-03-02"),
+		(3, "Rocky", "Reddy", 52.40, "2023-03-03"),
+		(4, "Rcky", "Bddy", 53.40, "2023-03-04"),
+        (5, "RIckey", "Beddy", 54.40, "2023-03-05"),
+        (6, "Rckiy", "Riddy", 55.40, "2023-03-06");
+
+SELECT * FROM employees;
+
+INSERT INTO employees (employee_id, first_name, last_name)
+VALUES  (7, "Reddappa", "MR");
+
+SELECT * FROM employees
